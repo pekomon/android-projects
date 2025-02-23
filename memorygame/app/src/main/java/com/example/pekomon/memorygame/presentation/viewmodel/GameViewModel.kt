0cb011fbf21f5cc472222f89235e78150fd57f36
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-23 11:15:42 +0200

    feat: add card flipping logic to GameViewModel

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/viewmodel/GameViewModel.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/viewmodel/GameViewModel.kt
@@ -3,11 +3,37 @@ package com.example.pekomon.memorygame.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.pekomon.memorygame.domain.repository.CardRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+
+data class MemoryCard(
+    val id: Int,
+    val imageRes: Int,
+    val isFlipped: Boolean = false
+)
 
 @HiltViewModel
 class GameViewModel @Inject constructor(
     private val cardRepository: CardRepository
 ) : ViewModel() {
-    val cards = cardRepository.getCards()
+    //val cardsX = cardRepository.getCards()
+    private val _cards = MutableStateFlow<List<MemoryCard>>(
+        cardRepository.getCards().map {
+            MemoryCard(id = it.id, imageRes = it.imageRes)
+        }
+    )
+    val cards = _cards.asStateFlow()
+
+
+    fun flipCard(cardId: Int) {
+        _cards.value = _cards.value.map { card ->
+            if (card.id == cardId) {
+                card.copy(isFlipped = !card.isFlipped)
+            } else {
+                card
+            }
+        }
+    }
 }
