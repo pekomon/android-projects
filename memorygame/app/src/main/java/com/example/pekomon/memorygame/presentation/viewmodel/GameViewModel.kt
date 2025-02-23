c84108784bdfeed5f56d76650ad67a49ae2666e6
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-23 11:34:07 +0200

    feat: add pair matching logic to GameViewModel

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/viewmodel/GameViewModel.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/viewmodel/GameViewModel.kt
@@ -1,38 +1,60 @@
 package com.example.pekomon.memorygame.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pekomon.memorygame.domain.model.Card
 import com.example.pekomon.memorygame.domain.repository.CardRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-data class MemoryCard(
-    val id: Int,
-    val imageRes: Int,
-    val isFlipped: Boolean = false
-)
 
 @HiltViewModel
 class GameViewModel @Inject constructor(
     private val cardRepository: CardRepository
 ) : ViewModel() {
-    //val cardsX = cardRepository.getCards()
-    private val _cards = MutableStateFlow<List<MemoryCard>>(
-        cardRepository.getCards().map {
-            MemoryCard(id = it.id, imageRes = it.imageRes)
-        }
+    private val _cards = MutableStateFlow(
+        cardRepository.getCards()
     )
     val cards = _cards.asStateFlow()
 
+    private var selectedCards = mutableListOf<Card>()
 
     fun flipCard(cardId: Int) {
-        _cards.value = _cards.value.map { card ->
-            if (card.id == cardId) {
-                card.copy(isFlipped = !card.isFlipped)
-            } else {
-                card
+        if (selectedCards.size >= 2) return
+
+        _cards.update { currentCards ->
+            currentCards.map { card ->
+                if (card.id == cardId && !card.isFlipped) {
+                    card.copy(isFlipped = true).also { selectedCards.add(it) }
+                } else card
+
+            }
+        }
+
+        if (selectedCards.size == 2) {
+            checkForMatch()
+        }
+    }
+
+    private fun checkForMatch() {
+        if (selectedCards[0].imageRes == selectedCards[1].imageRes) {
+            selectedCards.clear()
+        } else {
+            viewModelScope.launch {
+                delay(800)
+                _cards.update { currentCards ->
+                    currentCards.map { card ->
+                       if (selectedCards.any { it.id == card.id }) {
+                           card.copy(isFlipped = false)
+                       }
+                       else card
+                    }
+                }
+                selectedCards.clear()
             }
         }
     }
