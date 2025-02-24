ef58c9a395535f9fceecb63efd42f73d8b1cf9ea
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-24 20:49:05 +0200

    feat: display game win screen and restart option

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/ui/GameScreen.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/ui/GameScreen.kt
@@ -1,20 +1,26 @@
 package com.example.pekomon.memorygame.presentation.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -30,42 +36,59 @@ fun GameScreen(
     val cards by viewModel.cards.collectAsState()
     val moves by viewModel.moves.collectAsState()
     val score by viewModel.score.collectAsState()
+    val isGameWon by viewModel.isGameWon.collectAsState()
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Moves: $moves",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = "Score: $score",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(4),
-            modifier = Modifier.padding(16.dp)
-        ) {
-            items(cards) { card ->
-                Card(modifier = Modifier
-                    .size(80.dp)
-                    .padding(4.dp),
-                    onClick = { viewModel.flipCard(card.id) }
-                ) {
-                    if (card.isFlipped) {
-                        Image(
-                            painter = painterResource(id = card.imageRes),
-                            contentDescription = "Memory card",
-                        )
-                    } else {
-                        Image(
-                            painter = painterResource(id = R.drawable.card_back),
-                            contentDescription = "Card upside down",
-                        )
+        if (isGameWon) {
+            Text(
+                text = "\uD83C\uDF89 You Win! \uD83C\uDF89\"",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Green
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { viewModel.restartGame() }
+            ) {
+                Text(text = "Play Again")
+            }
+        } else {
+
+            Text(
+                text = "Moves: $moves",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Score: $score",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(4),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                items(cards) { card ->
+                    Card(modifier = Modifier
+                        .size(80.dp)
+                        .padding(4.dp),
+                        onClick = { viewModel.flipCard(card.id) }
+                    ) {
+                        if (card.isFlipped) {
+                            Image(
+                                painter = painterResource(id = card.imageRes),
+                                contentDescription = "Memory card",
+                            )
+                        } else {
+                            Image(
+                                painter = painterResource(id = R.drawable.card_back),
+                                contentDescription = "Card upside down",
+                            )
+                        }
                     }
                 }
             }
