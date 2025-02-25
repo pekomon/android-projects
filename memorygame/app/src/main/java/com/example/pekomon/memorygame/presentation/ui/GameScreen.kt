f8a09458e3f1cbd1561daedb8c9bffc1f9984941
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-25 19:39:50 +0200

    feat: display best score in GameScreen

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/ui/GameScreen.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/presentation/ui/GameScreen.kt
@@ -7,13 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -21,12 +19,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.pekomon.memorygame.R
 import com.example.pekomon.memorygame.presentation.ui.component.AnimatedMemoryCard
 import com.example.pekomon.memorygame.presentation.viewmodel.GameViewModel
 
@@ -38,6 +34,7 @@ fun GameScreen(
     val moves by viewModel.moves.collectAsState()
     val score by viewModel.score.collectAsState()
     val isGameWon by viewModel.isGameWon.collectAsState()
+    val bestScore by viewModel.bestScore.collectAsState()
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -59,6 +56,11 @@ fun GameScreen(
             }
         } else {
 
+            Text(
+                text = "Best Score: ${bestScore ?: "-"}",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
             Text(
                 text = "Moves: $moves",
                 fontSize = 20.sp,
