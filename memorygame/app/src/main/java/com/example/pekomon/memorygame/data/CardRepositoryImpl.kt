c63ff0f12940f2a321affeb22ca09cb0ed31569e
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-25 19:19:31 +0200

    feat: implement best score storage in CardRepositoryImpl

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/data/CardRepositoryImpl.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/data/CardRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package com.example.pekomon.memorygame.data
 
 import com.example.pekomon.memorygame.R
+import com.example.pekomon.memorygame.data.local.BestScoreDao
+import com.example.pekomon.memorygame.domain.model.BestScore
 import com.example.pekomon.memorygame.domain.model.Card
 import com.example.pekomon.memorygame.domain.repository.CardRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class CardRepositoryImpl @Inject constructor() : CardRepository {
+class CardRepositoryImpl @Inject constructor(
+    private val bestScoreDao: BestScoreDao
+) : CardRepository {
     override fun getCards(): List<Card> {
         val images = listOf(
             R.drawable.image1, R.drawable.image2, R.drawable.image3,R.drawable.image4,
@@ -15,6 +21,18 @@ class CardRepositoryImpl @Inject constructor() : CardRepository {
         return images.mapIndexed { index, resourceId ->
             Card(id = index, imageRes = resourceId)
 
+        }
+    }
+
+    override suspend fun getBestScore(): Int? {
+        return withContext(Dispatchers.IO) {
+            bestScoreDao.getBestScore()?.score
+        }
+    }
+
+    override suspend fun saveBestScore(score: Int) {
+        withContext(Dispatchers.IO) {
+            bestScoreDao.insertBestScore(BestScore(score = score))
         }
     }
 }
