4b69afde8d1a7927d32d88b05e0aa4002b985b92
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-25 19:27:03 +0200

    feat: add Room database dependencies into AppModule

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/di/AppModule.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.pekomon.memorygame.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.pekomon.memorygame.data.CardRepositoryImpl
+import com.example.pekomon.memorygame.data.local.AppDatabase
 import com.example.pekomon.memorygame.domain.repository.CardRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,4 +19,19 @@ abstract class AppModule {
     @Binds
     @Singleton
     abstract fun bindCardRepository(impl: CardRepositoryImpl): CardRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "memory_game_db"
+            ).build()
+        }
+
+        @Provides
+        fun provideBestScoreDao(database: AppDatabase) = database.bestScoreDao()
+    }
 }
