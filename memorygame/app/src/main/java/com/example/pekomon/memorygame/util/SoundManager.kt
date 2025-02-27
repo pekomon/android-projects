7bdcef6b05d9b100216c801a434dae923f2d4727
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-02-27 21:09:52 +0200

    fix: proper handling for win sound

--- memorygame/app/src/main/java/com/example/pekomon/memorygame/util/SoundManager.kt
+++ memorygame/app/src/main/java/com/example/pekomon/memorygame/util/SoundManager.kt
@@ -3,6 +3,7 @@ package com.example.pekomon.memorygame.util
 import android.content.Context
 import android.media.MediaPlayer
 import android.media.SoundPool
+import android.util.Log
 import com.example.pekomon.memorygame.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -15,7 +16,6 @@ class SoundManager @Inject constructor(
     private val soundPool: SoundPool = SoundPool.Builder().setMaxStreams(3).build()
     private val flipSoundId: Int = soundPool.load(context, R.raw.card_flip, 1)
     private val matchSoundId: Int = soundPool.load(context, R.raw.match_found, 1)
-    private val winSoundId = soundPool.load(context, R.raw.game_win, 1)
 
     private var winMediaPlayer: MediaPlayer? = null
     private var bgMediaPlayer: MediaPlayer? = null
@@ -32,21 +32,33 @@ class SoundManager @Inject constructor(
     }
 
     fun playWinSound() {
-        winMediaPlayer = MediaPlayer.create(context, winSoundId).apply {
-            setOnCompletionListener { it.release() }
+        winMediaPlayer = MediaPlayer.create(context, R.raw.game_win).apply {
+            setOnCompletionListener {
+                it.release()
+                winMediaPlayer = null
+            }
             setVolume(effectsVolume, effectsVolume)
             start()
         }
     }
 
     fun startBackgroundMusic() {
+
         if (bgMediaPlayer == null) {
             bgMediaPlayer = MediaPlayer.create(context, R.raw.background_music).apply {
                 isLooping = true
                 setVolume(musicVolume, musicVolume)
+                setOnErrorListener { mp, what, extra ->
+                    Log.e("SoundManager", "Error playing background music: what=$what, extra=$extra")
+                    stopBackgroundMusic()
+                    true
+                }
                 start()
             }
+        } else if (!bgMediaPlayer!!.isPlaying) {
+            bgMediaPlayer?.start()
         }
+
     }
 
     private fun stopBackgroundMusic() {
