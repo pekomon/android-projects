8b7a167c35d286bae646227ea1a6285e045f8ebe
Pekomon <5183385+pekomon@users.noreply.github.com>
2026-05-24 14:10:19 +0300

    Introduce market data result model

---
+++ CryptoApp/app/src/main/java/com/pekomon/cryptoapp/domain/market/MarketDataResult.kt
@@ -0,0 +1,35 @@
+package com.pekomon.cryptoapp.domain.market
+
+sealed interface MarketDataResult<out T> {
+    data class Success<T>(
+        val value: T
+    ) : MarketDataResult<T>
+
+    data class Failure(
+        val error: MarketDataError
+    ) : MarketDataResult<Nothing>
+}
+
+sealed interface MarketDataError {
+    val technicalMessage: String?
+
+    data class Unauthorized(
+        override val technicalMessage: String? = null
+    ) : MarketDataError
+
+    data class Forbidden(
+        override val technicalMessage: String? = null
+    ) : MarketDataError
+
+    data class RateLimited(
+        override val technicalMessage: String? = null
+    ) : MarketDataError
+
+    data class Network(
+        override val technicalMessage: String? = null
+    ) : MarketDataError
+
+    data class Unknown(
+        override val technicalMessage: String? = null
+    ) : MarketDataError
+}
